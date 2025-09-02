a1607c094ba363661a4207cd853c9c2d49b51341
BillieLurk <bill@1618holding.com>
2025-09-02 13:59:36 +0200

    fixes

--- lua/autoScheme/init.lua
+++ lua/autoScheme/init.lua
@@ -1,11 +1,44 @@
 local M = {}
 
+M.config = {
+	schemes = {},
+	default = "",
+}
+
 function M.setup(opts)
+	M.config = vim.tbl_deep_extend("force", M.config, opts or {})
 	opts = opts or {}
-	vim.api.nvim_create_autocmd("BufReadPost", {
+	vim.api.nvim_create_autocmd("BufEnter", {
 		callback = function(args)
+			local buftype = vim.bo[args.buf].buftype
 			local ft = vim.bo[args.buf].filetype
-			vim.notify("Opened " .. (vim.api.nvim_buf_get_name(args.buf) or "??") .. " with ft=" .. ft)
+
+			if buftype ~= "" then
+				return
+			end
+
+			if ft == nil or ft == "" then
+				return
+			end
+
+			local matched = false
+			for _, scheme in pairs(M.config.schemes) do
+				if ft == scheme.filetype then
+					local ok, _ = pcall(vim.cmd, "colorscheme " .. scheme.colorscheme)
+					if not ok then
+						print("Error: Cannot find color scheme '" .. scheme.colorscheme .. "'")
+					end
+					matched = true
+					break
+				end
+			end
+
+			if not matched then
+				local ok, _ = pcall(vim.cmd, "colorscheme " .. M.config.default)
+				if not ok then
+					print("Error: Cannot find default color scheme '" .. M.config.default .. "'")
+				end
+			end
 		end,
 	})
 end
